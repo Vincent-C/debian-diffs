31c5ad70342b4439559c6e31a7149406cf06d20a
Vincent Cheng <Vincentc1208@gmail.com>
2013-08-15 22:55:42 -0700

    Update java-common to 0.49+vc1

---
+++ java-common/debian/java_defaults.mk
@@ -0,0 +1,23 @@
+
+# makefile fragment to define the macros java_default_version,
+# java{,5,6,7,8}_architectures
+
+java8_architectures =
+java7_architectures = alpha amd64 armel armhf arm64 i386 ia64 lpia \
+		powerpc ppc64 s390x sh4 sparc sparc64
+java6_architectures = $(java7_architectures) \
+		mips mipsel powerpcspe s390
+java5_architectures = $(java6_architectures) \
+		hppa m68k x32 kfreebsd-amd64 kfreebsd-i386 hurd-i386
+java_architectures = $(java5_architectures)
+
+_java_host_arch := $(if $(DEB_HOST_ARCH),$(DEB_HOST_ARCH),$(shell dpkg-architecture -qDEB_HOST_ARCH))
+ifneq (,$(filter $(_java_host_arch),$(java8_architectures)))
+  java_default_version = 8
+else ifneq (,$(filter $(_java_host_arch),$(java6_architectures)))
+  java_default_version = 7
+else ifneq (,$(filter $(_java_host_arch),$(java6_architectures)))
+  java_default_version = 6
+else ifneq (,$(filter $(_java_host_arch),$(java5_architectures)))
+  java_default_version = 5
+endif
