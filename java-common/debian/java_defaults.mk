3ac83ca9760e0c87cc054f5aa4f7b43659b608ca
Vincent Cheng <vcheng@debian.org>
2015-09-13 23:24:41 -0700

    rebase java-common on top of 0.53 in Debian

--- java-common/debian/java_defaults.mk
+++ java-common/debian/java_defaults.mk
@@ -1,12 +1,15 @@
 
 # makefile fragment to define the macros java_default_version,
-# java{,5,6,7,8}_architectures
+# java{,5,6,7,8,9}_architectures
 
-java8_architectures = amd64 i386
-java7_architectures = alpha amd64 arm64 armel armhf arm64 i386 lpia \
-		mips mipsel mips64el \
+java9_architectures =
+java8_architectures = $(java9_architectures) \
+		amd64 arm64 armel armhf i386 \
 		powerpc powerpcspe ppc64 ppc64el \
-		s390x sh4 x32
+		s390x x32
+java7_architectures = $(java8_architectures) \
+		alpha lpia \
+		mips mipsel mips64el sh4
 java6_architectures = $(java7_architectures)
 java5_architectures = $(java6_architectures) \
 		hppa ia64 m68k s390 sparc sparc64 \
@@ -14,7 +17,9 @@ java5_architectures = $(java6_architectures) \
 java_architectures = $(java5_architectures)
 
 _java_host_arch := $(if $(DEB_HOST_ARCH),$(DEB_HOST_ARCH),$(shell dpkg-architecture -qDEB_HOST_ARCH))
-ifneq (,$(filter $(_java_host_arch),$(java8_architectures)))
+ifneq (,$(filter $(_java_host_arch),$(java9_architectures)))
+  java_default_version = 9
+else ifneq (,$(filter $(_java_host_arch),$(java8_architectures)))
   java_default_version = 8
 else ifneq (,$(filter $(_java_host_arch),$(java6_architectures)))
   java_default_version = 7
